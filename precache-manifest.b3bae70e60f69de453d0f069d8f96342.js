194d418d847bb66beb6a69df85a09d538a811fb5
Ruslan.Belous <r.bilous@viseven.com>
2022-07-19 09:47:31 +0300

    deploy

---
+++ precache-manifest.b3bae70e60f69de453d0f069d8f96342.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86cca73badc4e13f228f",
+    "url": "/test_todo_layout/css/app.805b4ef1.css"
+  },
+  {
+    "revision": "6b364970cb94e3843bb95621d9074c88",
+    "url": "/test_todo_layout/fonts/Montserrat-Medium.6b364970.woff"
+  },
+  {
+    "revision": "5ed489077dde2ddf998d00f0690aeb61",
+    "url": "/test_todo_layout/fonts/Roboto-Bold.5ed48907.woff"
+  },
+  {
+    "revision": "9a3bf7acae14d9b5ed5a88458106b58b",
+    "url": "/test_todo_layout/fonts/Roboto-Medium.9a3bf7ac.woff"
+  },
+  {
+    "revision": "99433f7d92c3a5c1179fa7ebb1ba0a48",
+    "url": "/test_todo_layout/fonts/Roboto-Regular.99433f7d.woff"
+  },
+  {
+    "revision": "901a8b788bd4c7cb0cbd50fc9e96e3ae",
+    "url": "/test_todo_layout/img/background.901a8b78.png"
+  },
+  {
+    "revision": "a87ce9a0c207a78522d49f883c0332e1",
+    "url": "/test_todo_layout/index.html"
+  },
+  {
+    "revision": "86cca73badc4e13f228f",
+    "url": "/test_todo_layout/js/app.21a5a14f.js"
+  },
+  {
+    "revision": "6d27563aef9f4368f84a",
+    "url": "/test_todo_layout/js/chunk-vendors.251a1d12.js"
+  },
+  {
+    "revision": "250fd24233d12e1e78282aad1b23a380",
+    "url": "/test_todo_layout/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/test_todo_layout/robots.txt"
+  }
+]);
